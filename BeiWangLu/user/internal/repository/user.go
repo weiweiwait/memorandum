054faee7e9e001996407c1d8024a398dd10ff7d3
fansg666 <2680315178@qq.com>
2024-03-18 22:50:43 +0800

    protoc生成代码

---
+++ BeiWangLu/user/internal/repository/user.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"github.com/CocaineCong/grpc-todolist/consts"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	UserID         int64  `gorm:"primarykey"`
+	UserName       string `gorm:"unique"`
+	NickName       string
+	PasswordDigest string
+}
+
+func (*User) TableName() string {
+	return "user"
+}
+
+// SetPassword 加密密码
+func (user *User) SetPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), consts.PassWordCost)
+	if err != nil {
+		return err
+	}
+	user.PasswordDigest = string(bytes)
+	return nil
+}
+
+// CheckPassword 检验密码
+func (user *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordDigest), []byte(password))
+	return err == nil
+}
